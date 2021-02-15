a590ea58ba6c6356b5ff4bd0d7849114fa1bb68b
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2021-02-14 23:21:52 -0500

    set up eslint, prettier, tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('you code goes here, huzzah!');
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} = {
+	name: 'Woodie Westbrook',
+	age: 46,
+	hobbies: ['cycling', 'gaming', 'madlibs', 'ww'],
+	role: [1, 'admin']
+};
+
+// person.role.push('woodie')
+
+// eslint-disable-next-line no-console
+console.log(person);
+person.hobbies.forEach((x) => {
+	console.log(x);
+});
---
+++ src/basics.ts
@@ -0,0 +1,8 @@
+function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+const number1: number = 5;
+const number2 = 10;
+
+console.log(add(number1, number2));
